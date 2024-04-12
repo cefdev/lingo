8410e4452cd985bc4084353f8f85df9a59c572ae
Mounssif Koumila <moncef.koumila@gmail.com>
2024-04-12 20:14:06 +0000

    Feat: creating button component variants

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,20 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <div className="text-green-500 font-bold">
-      Hello Lingo !
+    <div className="p-4 flex flex-col gap-4 max-w-[250px]">
+      <Button>Default</Button>
+      <Button variant="primary">Primary</Button>
+      <Button variant="primaryOutline">Primary Outline</Button>
+      <Button variant="secondary">Secondary</Button>
+      <Button variant="secondaryOutline">Secondary Outline</Button>
+      <Button variant="danger">Danger</Button>
+      <Button variant="dangerOutline">Danger Outline</Button>
+      <Button variant="super">Super</Button>
+      <Button variant="superOutline">Super Outline</Button>
+      <Button variant="ghost">Ghost</Button>
+      <Button variant="sidebar">Sidebar</Button>
+      <Button variant="sidebarOutline">Sidebar Outline</Button>
     </div>
   );
 }
