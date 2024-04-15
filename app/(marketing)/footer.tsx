bd2782ab3cfbfa7802f05ba98f30125f771c0222
Mounssif Koumila <moncef.koumila@gmail.com>
2024-04-15 23:40:35 +0100

    feat: styling footer

--- app/(marketing)/footer.tsx
+++ app/(marketing)/footer.tsx
@@ -1,7 +1,63 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
 export const Footer = () => {
   return (
     <footer className="hidden lg:block h-20 w-full border-t-2 border-slate-200 p-2">
-      Footer
+      <div className="max-w-screen-lg mx-auto flex items-center justify-evenly h-full">
+        <div className="max-w-screen-lg mx-auto flex items-center justify-evenly h-full">
+          <Button size="lg" variant="ghost" className="w-full">
+            <Image
+              src="/flags/mr.svg"
+              alt="Arabic"
+              height={32}
+              width={40}
+              className="mr-4 rounded-md"
+            />
+            Arabic
+          </Button>
+          <Button size="lg" variant="ghost" className="w-full">
+            <Image
+              src="/flags/es.svg"
+              alt="Spanish"
+              height={32}
+              width={40}
+              className="mr-4 rounded-md"
+            />
+            Spanish
+          </Button>
+          <Button size="lg" variant="ghost" className="w-full">
+            <Image
+              src="/flags/fr.svg"
+              alt="French"
+              height={32}
+              width={40}
+              className="mr-4 rounded-md"
+            />
+            French
+          </Button>
+          <Button size="lg" variant="ghost" className="w-full">
+            <Image
+              src="/flags/it.svg"
+              alt="Italian"
+              height={32}
+              width={40}
+              className="mr-4 rounded-md"
+            />
+            Italian
+          </Button>
+          <Button size="lg" variant="ghost" className="w-full">
+            <Image
+              src="/flags/kr.svg"
+              alt="Korean"
+              height={32}
+              width={40}
+              className="mr-4 rounded-md"
+            />
+            Korean
+          </Button>
+        </div>
+      </div>
     </footer>
   );
 };
--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -21,40 +21,42 @@ export default function Home() {
         <h1 className="text-xl lg:text-3xl font-bold text-neutral-600 max-w-[480px] text-center">
           Learn, practice and master new languages with Lingo.
         </h1>
-        {/* Show a loader while Clerk is loading */}
-        <ClerkLoading>
-          <Loader className="h-5 w-5 text-muted-foreground animate-spin" />
-        </ClerkLoading>
-        {/* Show Auth components after Clerk has been loaded */}
-        <ClerkLoaded>
-          {/* Show SignIn and SignUp buttons if the user hasn't signed in yet */}
-          <SignedOut>
-            <SignUpButton
-              mode="modal"
-              afterSignInUrl="/learn"
-              afterSignUpUrl="/learn"
-            >
-              <Button size="lg" variant="secondary" className="w-full">
-                Get Starter
+        <div className="flex flex-col items-center gap-y-3 max-w-[330px] w-full">
+          {/* Show a loader while Clerk is loading */}
+          <ClerkLoading>
+            <Loader className="h-5 w-5 text-muted-foreground animate-spin" />
+          </ClerkLoading>
+          {/* Show Auth components after Clerk has been loaded */}
+          <ClerkLoaded>
+            {/* Show SignIn and SignUp buttons if the user hasn't signed in yet */}
+            <SignedOut>
+              <SignUpButton
+                mode="modal"
+                afterSignInUrl="/learn"
+                afterSignUpUrl="/learn"
+              >
+                <Button size="lg" variant="secondary" className="w-full">
+                  Get Starter
+                </Button>
+              </SignUpButton>
+              <SignInButton
+                mode="modal"
+                afterSignInUrl="/learn"
+                afterSignUpUrl="/learn"
+              >
+                <Button size="lg" variant="primaryOutline" className="w-full">
+                  I already have an account
+                </Button>
+              </SignInButton>
+            </SignedOut>
+            {/* Show button to Learning page if the user is already signed in */}
+            <SignedIn>
+              <Button size="lg" variant="secondary" className="w-full" asChild>
+                <Link href="/learn">Continue Learning</Link>
               </Button>
-            </SignUpButton>
-            <SignInButton
-              mode="modal"
-              afterSignInUrl="/learn"
-              afterSignUpUrl="/learn"
-            >
-              <Button size="lg" variant="primaryOutline" className="w-full">
-                I already have an account
-              </Button>
-            </SignInButton>
-          </SignedOut>
-          {/* Show button to Learning page if the user is already signed in */}
-          <SignedIn>
-            <Button size="lg" variant="secondary" className="w-full" asChild>
-              <Link href="/learn">Continue Learning</Link>
-            </Button>
-          </SignedIn>
-        </ClerkLoaded>
+            </SignedIn>
+          </ClerkLoaded>
+        </div>
       </div>
     </div>
   );
